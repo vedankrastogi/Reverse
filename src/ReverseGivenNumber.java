682cb689d7d237453424e87350181637f1a147f6
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-10 23:58:27 +0530

    pallendron

--- src/ReverseGivenNumber.java
+++ src/ReverseGivenNumber.java
@@ -1,11 +1,22 @@
 public class ReverseGivenNumber {
     public static void main(String[] args) {
         int a=12345;
-        int b;
+        int c;
+        c=a;
+        int b;int s=0;
         while(a>0){
             b=a%10;
+            s=s+b;
             a=a/10;
-            System.out.print(b);
+
+        }
+        if(s==c){
+            System.out.println("pallendrom"
+                    );
+        }
+        else
+        {
+            System.out.println("not pallendron");
         }
 
 
